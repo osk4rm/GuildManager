ff05510afbae31c06b6a6ff80d798b24dfdfb2f0
Oskar Mokrzycki <oskar.mokrzycki@digit-al.pl>
2023-01-04 23:03:54 +0100

    added data annotation validation to register page (due to fluentvalidation error), todo: fix fluentvalidation in blazor wasm

--- GuildManager_Models/RegisterUserDto.cs
+++ GuildManager_Models/RegisterUserDto.cs
@@ -1,10 +1,16 @@
-﻿namespace GuildManager_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuildManager_Models
 {
     public class RegisterUserDto
     {
+        [Required, EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Nickname { get; set; }
+        [Required, StringLength(50, MinimumLength = 6)]
         public string Password { get; set; }
+        [Compare("Password",ErrorMessage ="Password and confirm password must match.")]
         public string ConfirmPassword { get; set; }
         public int RoleId { get; } = 1;
     }

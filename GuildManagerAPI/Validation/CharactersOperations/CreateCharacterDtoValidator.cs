99a683ec428eb3d8249cb1ec00d73bf7d0baaefa
Oskar Mokrzycki <oskar.mokrzycki@digit-al.pl>
2023-01-19 12:18:44 +0100

    createcharacterdto validator

--- GuildManagerAPI/Validation/CharactersOperations/CreateCharacterDtoValidator.cs
+++ GuildManagerAPI/Validation/CharactersOperations/CreateCharacterDtoValidator.cs
@@ -8,7 +8,7 @@ namespace GuildManagerAPI.Validation.CharactersOperations
     {
         public CreateCharacterDtoValidator(GuildManagerDbContext dbContext)
         {
-            RuleFor(x=>x.Name)
+            RuleFor(x => x.Name)
                 .Custom((value, context) =>
                 {
                     var nameInUse = dbContext.Characters.Any(u => u.Name == value);
@@ -17,7 +17,38 @@ namespace GuildManagerAPI.Validation.CharactersOperations
                         context.AddFailure("Name", "Character with this name already exists.");
                     }
                 })
-                .NotEmpty().WithMessage("Character name cannot be empty");
+                .NotEmpty().WithMessage("Character name cannot be empty")
+                .NotNull().WithMessage("Character name cannot be empty");
+
+            RuleFor(x => x.ClassId)
+                .NotNull().WithMessage("Class cannot be empty")
+                .Custom((value, context) =>
+                {
+                    var classExists = dbContext.CharacterClasses.Any(c => c.Id == value);
+                    if (!classExists)
+                    {
+                        context.AddFailure($"Class with id {value} does not exist in database.");
+                        
+                    }
+                });
+
+            RuleFor(x => new { x.ClassId, x.ClassSpecializationId })
+                .Custom((value, context) =>
+                {
+                    var givenSpec = dbContext.ClassSpecializations.FirstOrDefault(s => s.Id == value.ClassSpecializationId);
+                    if(givenSpec is null)
+                    {
+                        context.AddFailure($"Class specialization with id {value.ClassSpecializationId} does not exist in database.");
+                    }
+                    else if (givenSpec.CharacterClassId != value.ClassId)
+                    {
+                        context.AddFailure($"Class specialization is not the specialization of class with id {value.ClassId}");
+                    }
+                });
+                
+
+
+
         }
     }
 }

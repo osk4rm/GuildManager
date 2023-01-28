9e08e1f09860d443dbfd2dd6f8d5164ae018fbb1
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-01-28 23:38:41 +0100

    base modal for adding new events, but it needs some cool design

---
+++ GuildManager_WebApp/Services/ExpansionsAndLocationsService/ExpansionsLocationsService.cs
@@ -0,0 +1,35 @@
+﻿using GuildManager_Models;
+using GuildManager_Models.RaidEvents;
+using GuildManager_Models.RaidExpansionsAndLocations;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace GuildManager_WebApp.Services.ExpansionsAndLocationsService
+{
+    public class ExpansionsLocationsService : IExpansionsLocationsService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExpansionsLocationsService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ServiceResponse<List<RaidExpansionDto>>> GetAllExpansions()
+        {
+            var response = await _httpClient.GetAsync("api/expansions");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ServiceResponse<List<RaidExpansionDto>>>(responseContent);
+
+            return result;
+        }
+
+        public async Task<ServiceResponse<List<RaidLocationDto>>> GetAllLocations()
+        {
+            var response = await _httpClient.GetAsync("api/raid-locations");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ServiceResponse<List<RaidLocationDto>>>(responseContent);
+
+            return result;
+        }
+    }
+}
---
+++ GuildManager_WebApp/Services/ExpansionsAndLocationsService/IExpansionsLocationsService.cs
@@ -0,0 +1,12 @@
+﻿using GuildManager_Models.RaidEvents;
+using GuildManager_Models;
+using GuildManager_Models.RaidExpansionsAndLocations;
+
+namespace GuildManager_WebApp.Services.ExpansionsAndLocationsService
+{
+    public interface IExpansionsLocationsService
+    {
+        Task<ServiceResponse<List<RaidExpansionDto>>> GetAllExpansions();
+        Task<ServiceResponse<List<RaidLocationDto>>> GetAllLocations();
+    }
+}

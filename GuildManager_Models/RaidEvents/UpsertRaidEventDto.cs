3f2d395a7174bc6b2e9f9e676da646c369105e6b
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-01-29 09:17:47 +0100

    improved addraidevent modal design

--- GuildManager_Models/RaidEvents/UpsertRaidEventDto.cs
+++ GuildManager_Models/RaidEvents/UpsertRaidEventDto.cs
@@ -10,8 +10,8 @@ namespace GuildManager_Models.RaidEvents
     public class UpsertRaidEventDto
     {
         public int RaidLocationId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; } = DateTime.Now;
         public string? Description { get; set; }
         public bool AutoAccept { get; set; } = false;
         public RaidDifficulty RaidDifficulty { get; set; }

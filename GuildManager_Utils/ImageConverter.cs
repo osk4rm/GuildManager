fa7906c478b30b7f5d4cc057f00513022311c9b4
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-02-04 23:09:47 +0100

    utils project -> getmimetypefrombyte[]

---
+++ GuildManager_Utils/ImageConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace GuildManager_Utils
+{
+    public class ImageConverter
+    {
+        public static string GetMimeTypeFromByteArray(byte[] bytes)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                Image image = Image.FromStream(ms);
+                ImageFormat format = image.RawFormat;
+                string mimeType = format.ToString();
+                return mimeType;
+            }
+        }
+    }
+}

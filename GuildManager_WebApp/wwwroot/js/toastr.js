cee15f54f7b4517534e070d23940773e0a7c25a1
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-02-07 00:15:00 +0100

    toastr notifications after changing avatar

--- GuildManager_WebApp/wwwroot/js/toastr.js
+++ GuildManager_WebApp/wwwroot/js/toastr.js
@@ -1,9 +1,9 @@
-﻿window.ShowNotification = (type, message) => {
+﻿window.ShowNotification=(type, message) => {
     if (type === "success") {
-        toastr.success(message, "Success!", {timeOut: 5000})
+        toastr.success(message, "Success!", { timeOut: 5000 });
     }
 
     if (type === "error") {
-        toastr.error(message, "Error :(", { timeOut: 5000 })
+        toastr.error(message, "Error :(", { timeOut: 5000 });
     }
 }

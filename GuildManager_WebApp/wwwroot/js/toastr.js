df7174fe5dcc7ea2ff7dca2fc25d3f0fde443010
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-02-06 23:44:40 +0100

    toastr for notifications

---
+++ GuildManager_WebApp/wwwroot/js/toastr.js
@@ -0,0 +1,9 @@
+﻿window.ShowNotification = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Success!", {timeOut: 5000})
+    }
+
+    if (type === "error") {
+        toastr.error(message, "Error :(", { timeOut: 5000 })
+    }
+}

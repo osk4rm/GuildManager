51e6d867159dbaae0268e956eacd54f72790cc83
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-02-22 23:38:35 +0100

    first test

---
+++ GuildManager_UnitTests/DashTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using GuildManager_WebApp.Pages.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildManager_UnitTests
+{
+    public class DashTests
+    {
+        [Fact]
+        public void CalculateThisWeekFilter_ReturnsCorrectFilter()
+        {
+            // Arrange
+            var expectedStartDate = DateTime.Now.AddDays(((int)DayOfWeek.Wednesday - (int)DateTime.Now.DayOfWeek + 7) % 7 - 7).Date;
+            var expectedEndDate = expectedStartDate.AddDays(7).Date;
+            var expectedFilter = $"StartDate>{expectedStartDate.ToString("yyyy-MM-dd")},StartDate<{expectedEndDate.ToString("yyyy-MM-dd")}";
+
+            // Act
+            Dash dash = new Dash();
+            var result = dash.CalculateThisWeekFilter();
+
+            // Assert
+            result.Should().Be(expectedFilter);
+        }
+    }
+}

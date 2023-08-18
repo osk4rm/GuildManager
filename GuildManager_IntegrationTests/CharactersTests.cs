48e1c7eefe03413306529fd3d90cd7a69f47ea3e
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-08-18 16:04:43 +0200

    more integration testing

---
+++ GuildManager_IntegrationTests/CharactersTests.cs
@@ -0,0 +1,76 @@
+﻿using GuildManager_DataAccess;
+using GuildManager_Models;
+using GuildManager_Models.Characters;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace GuildManager_IntegrationTests
+{
+    public class CharactersTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private HttpClient _httpClient;
+
+        public CharactersTests(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContextOptions = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<GuildManagerDbContext>));
+
+                        services.Remove(dbContextOptions);
+                        services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+
+                        services.AddMvc(opts => opts.Filters.Add(new FakeUserFilter()));
+
+                        services.AddDbContext<GuildManagerDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("GMTestDB");
+                        });
+                    });
+                })
+                .CreateClient();
+        }
+
+        [Fact]
+        public async Task GetUserCharacters_WithNoAuthentication_ReturnsUnauthorizedResult()
+        {
+            //arrange
+            
+
+            //act
+            var response = await _httpClient.GetAsync("/api/v2/Characters/GetUserCharacters");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Unauthorized);
+        }
+
+        //ZWRACA USER NOT FOUND (404), TODO: zrobić seedowanie fake'owej bazy
+        [Fact]
+        public async Task Create_WithValidModel_ReturnsOkWithResultId()
+        {
+            //arrange 
+            var model = new CreateCharacterDto()
+            {
+                Name = "TestCharacter",
+                ItemLevel = 350,
+                ClassSpecializationId = 39,
+                ClassId = 14
+            };
+            var json = JsonConvert.SerializeObject(model);
+            var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            //act
+            var response = await _httpClient.PostAsync("/api/v2/Characters/Create", httpContent);
+            var contentString = await response.Content.ReadAsStringAsync();
+            var content = JsonConvert.DeserializeObject<ServiceResponse<int?>>(contentString);
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            content.Data.Should().NotBeNull();
+        }
+    }
+}
--- GuildManager_IntegrationTests/ClassesTests.cs
+++ GuildManager_IntegrationTests/ClassesTests.cs
@@ -1,41 +1,28 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using GuildManager_Models.Characters;
+using Microsoft.AspNetCore.Hosting;
 using System.Net;
 
 namespace GuildManager_IntegrationTests
 {
     public class ClassesTests : IClassFixture<WebApplicationFactory<Program>>
     {
-        private readonly WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
 
         public ClassesTests(WebApplicationFactory<Program> factory)
         {
-            _factory = factory;
+            _client = factory.CreateClient();
         }
 
-        [Fact]
-        public async Task GetAllClasses_ReturnsOkResponse()
-        {
-            // Arrange
-            var client = _factory.CreateClient();
-            var expectedStatusCode = HttpStatusCode.OK;
-
-            // Act
-            var response = await client.GetAsync("/api/v2/Classes/GetAll");
-
-            // Assert
-            Assert.Equal(expectedStatusCode, response.StatusCode);
-        }
         [Fact]
         public async Task GetAllClasses_ReturnsOkResult()
         {
-            var application = new WebApplicationFactory<Program>();
-            var client = application.CreateClient();
 
-            var response = await client.GetAsync("/api/v2/Classes/GetAll");
-
+            var response = await _client.GetAsync("/api/v2/Classes/GetAll");
+           
             //assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
 
+        
     }
 }

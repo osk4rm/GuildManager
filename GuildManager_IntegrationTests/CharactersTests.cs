152241cb74c25cd3dbcc8e0109ffcbd7af17aaa4
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-08-19 07:40:36 +0200

    more integration tests

--- GuildManager_IntegrationTests/CharactersTests.cs
+++ GuildManager_IntegrationTests/CharactersTests.cs
@@ -1,9 +1,12 @@
 ﻿using GuildManager_DataAccess;
+using GuildManager_DataAccess.Entities;
+using GuildManager_IntegrationTests.Helpers;
 using GuildManager_Models;
 using GuildManager_Models.Characters;
 using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System.Net.Http;
 using System.Text;
 
 namespace GuildManager_IntegrationTests
@@ -11,10 +14,12 @@ namespace GuildManager_IntegrationTests
     public class CharactersTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private HttpClient _httpClient;
+        private TestSeeder _seeder;
+        private readonly WebApplicationFactory<Program> _factory;
 
         public CharactersTests(WebApplicationFactory<Program> factory)
         {
-            _httpClient = factory
+            _factory = factory
                 .WithWebHostBuilder(builder =>
                 {
                     builder.ConfigureServices(services =>
@@ -31,8 +36,10 @@ namespace GuildManager_IntegrationTests
                             options.UseInMemoryDatabase("GMTestDB");
                         });
                     });
-                })
-                .CreateClient();
+                });
+
+            _httpClient = _factory.CreateClient();
+            _seeder = new TestSeeder(_factory);
         }
 
         [Fact]
@@ -57,20 +64,91 @@ namespace GuildManager_IntegrationTests
             {
                 Name = "TestCharacter",
                 ItemLevel = 350,
-                ClassSpecializationId = 39,
-                ClassId = 14
+                ClassSpecializationId = 1,
+                ClassId = 1
             };
-            var json = JsonConvert.SerializeObject(model);
-            var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            var user = new User()
+            {
+                Id = 1,
+                Email = "test@user.com",
+                Nickname = "testuser",
+                PasswordHash = "mysecretpassword"
+            };
+
+            await _seeder.SeedEntityAsync(user);
 
             //act
-            var response = await _httpClient.PostAsync("/api/v2/Characters/Create", httpContent);
+            var response = await _httpClient.PostAsync("/api/v2/Characters/Create", model.ToJsonHttpContent());
             var contentString = await response.Content.ReadAsStringAsync();
             var content = JsonConvert.DeserializeObject<ServiceResponse<int?>>(contentString);
 
             //assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
             content.Data.Should().NotBeNull();
+        }
+
+        //ZWRACA USER NOT FOUND (404), TODO: zrobić seedowanie fake'owej bazy
+        [Fact]
+        public async Task Create_WithInvalidModel_ReturnsBadRequest()
+        {
+            //arrange
+            var model = new CreateCharacterDto()
+            {
+                Name = "TestCharacter",
+                ItemLevel = 350,
+                ClassSpecializationId = 1,
+                ClassId = 1
+            };
+            var user = new User()
+            {
+                Id = 1,
+                Email = "test@user.com",
+                Nickname = "testuser",
+                PasswordHash = "mysecretpassword"
+            };
+
+            await _seeder.SeedEntityAsync(user);
+            
+            //act
+            var response = await _httpClient.PostAsync("/api/v2/Characters/Create", model.ToJsonHttpContent());
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Delete_ForNonExistingCharacter_ReturnsNotFound()
+        {
+            //arrange
+
+            //act
+            var response = await _httpClient.DeleteAsync($"/api/v2/Characters/Delete?id=0");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Delete_ForExistingCharacter_ReturnsOk()
+        {
+            //arrange
+            var character = new Character()
+            {
+                ClassSpecializationId = 39,
+                ClassId = 14,
+                Id = 1,
+                Name = "Test",
+                UserId = 1
+            };
+
+            await _seeder.SeedEntityAsync(character);
+
+            //act
+            var response = await _httpClient.DeleteAsync($"/api/v2/Characters/Delete?id={character.Id}");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
     }
 }
---
+++ GuildManager_IntegrationTests/Helpers/TestSeeder.cs
@@ -0,0 +1,22 @@
+﻿using GuildManager_DataAccess;
+
+namespace GuildManager_IntegrationTests.Helpers
+{
+    public class TestSeeder
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public TestSeeder(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+        public async Task SeedEntityAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetService<GuildManagerDbContext>();
+            dbContext.Add(entity);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}

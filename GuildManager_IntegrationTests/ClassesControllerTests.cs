961e2bbcdc59822bb8ff093a9d05c02dc1bd0253
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-08-19 22:51:38 +0200

    ClassesControllerTests finished

--- GuildManager_IntegrationTests/ClassesControllerTests.cs
+++ GuildManager_IntegrationTests/ClassesControllerTests.cs
@@ -4,25 +4,40 @@ using System.Net;
 
 namespace GuildManager_IntegrationTests
 {
-    public class ClassesControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    public class ClassesControllerTests : BaseControllerTests
     {
-        private HttpClient _client;
+        public ClassesControllerTests(WebApplicationFactory<Program> factory) : base(factory) { }
 
-        public ClassesControllerTests(WebApplicationFactory<Program> factory)
-        {
-            _client = factory.CreateClient();
-        }
 
         [Fact]
         public async Task GetAllClasses_ReturnsOkResult()
         {
-
-            var response = await _client.GetAsync("/api/v2/Classes/GetAll");
+            //act
+            var response = await _httpClient.GetAsync("/api/v2/Classes/GetAll");
            
             //assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
 
-        
+        [Fact]
+        public async Task GetClassCount_ReturnsOkResult()
+        {
+            //act
+            var response = await _httpClient.GetAsync("/api/v2/Classes/GetClassCount");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetRoleCount_ReturnsOkResult()
+        {
+            //act
+            var response = await _httpClient.GetAsync("/api/v2/Classes/GetRoleCount");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
     }
 }

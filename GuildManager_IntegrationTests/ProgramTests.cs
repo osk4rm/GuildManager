68a901628c5ae2bda8ef0c0bd46acf4a1d3c7564
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-08-20 08:18:44 +0200

    di tests

---
+++ GuildManager_IntegrationTests/ProgramTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuildManager_IntegrationTests
+{
+    public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private List<Type> _controllerTypes;
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProgramTests(WebApplicationFactory<Program> factory)
+        {
+            _controllerTypes = typeof(Program)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+            _factory = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        _controllerTypes.ForEach(c => services.AddScoped(c));
+                    });
+                });
+        }
+
+        [Fact]
+        public void ConfigureServices_ForControllers_RegistersAllDependencies()
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+
+            //assert
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetService(t);
+                controller.Should().NotBeNull();
+            });
+        }
+    }
+}

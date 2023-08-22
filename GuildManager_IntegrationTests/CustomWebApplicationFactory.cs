2aa3adc3169829775d22aa57f92f3af946e51f2a
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-08-22 23:31:15 +0200

    base setup for testcontainers

---
+++ GuildManager_IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using GuildManager_DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace GuildManager_IntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        private readonly string _connectionString;
+
+        public CustomWebApplicationFactory(DatabaseTests fixture)
+        {
+            _connectionString = fixture.GetConnectionString();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.Remove(services.SingleOrDefault(service => typeof(DbContextOptions<GuildManagerDbContext>) == service.ServiceType));
+                services.Remove(services.SingleOrDefault(service => typeof(DbConnection) == service.ServiceType));
+                services.AddDbContext<GuildManagerDbContext>((_, option) => option.UseSqlServer(_connectionString));
+            });
+        }
+    }
+}
---
+++ GuildManager_IntegrationTests/DatabaseTests.cs
@@ -0,0 +1,24 @@
+﻿using Testcontainers.MsSql;
+
+namespace GuildManager_IntegrationTests
+{
+    public sealed class DatabaseTests : IAsyncLifetime
+    {
+        private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder().Build();
+
+        public Task InitializeAsync()
+        {
+            return _msSqlContainer.StartAsync();
+        }
+
+        public Task DisposeAsync()
+        {
+            return _msSqlContainer.DisposeAsync().AsTask();
+        }
+
+        public string GetConnectionString()
+        {
+            return _msSqlContainer.GetConnectionString();
+        }
+    }
+}
